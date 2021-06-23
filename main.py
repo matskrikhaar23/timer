32256e3c0d6d907c2018915e3ddc94a0832b6ff5
matskrikhaar23 <75498783+matskrikhaar23@users.noreply.github.com>
2021-06-23 13:33:14 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -5,16 +5,34 @@
 #Compute how many breaks, and how long they will study
 import time
 
+
+
 def timer():
-    print("You will get a 5 minute break every 25 minutes, with a 15 minute break every hour.")
-    print(timeTotal)
+    print("test")
+    
+ 
+def start():
+            start = input("")
+            if (start == "" or start == " "):
+             timer()
+            else:
+             print("Please press space to start.")
+             start()   
+
+def breakCalc():
+    #print(timeTotal)
     breakAmt = int(timeTotal / 30)
     longBreakAmt = int(breakAmt / 4)
-    print(breakAmt)
+    #print(breakAmt)
     if (breakAmt >= 4):
         print("You will have", breakAmt, "breaks every twenty-five minutes, where", longBreakAmt, "of them is a 15 minute break, and the rest are five.")
+        print("Press enter to start the timer")
+        start() 
     elif (breakAmt < 4):
         print("You will have", breakAmt, "five minute breaks every 25 minutes.")
+        print("Press enter to start the timer")
+        start()
+
 def setup():
     print('Hello and welcome to the Study Pomodoro Timer! How long are you studying for?')
     studyTimeHours = int(input("Hours: ")) 
@@ -36,13 +54,14 @@ def setup():
         plural3 = "second"
     else:
         plural3 = "seconds"
-    global timeTotal
+    
     timeTotal = studyTimeMinutes  + (studyTimeHours * 60)
+    
     
     print("Okay! You want to study for ", studyTimeHours, plural, ",", studyTimeMinutes, plural2, ", and", studyTimeSeconds, plural3, "right?")
     confirmation = input("Yes or No?").lower()
     if confirmation in ["yes", "y"]: 
-        timer()
+        breakCalc()
     elif confirmation in ["no", "n"]:
         print("Alright then, let's do this again.")
         setup()
