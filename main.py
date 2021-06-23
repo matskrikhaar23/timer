98cc12da7c9e4ef1e525de81b116ed0bdff41036
matskrikhaar23 <75498783+matskrikhaar23@users.noreply.github.com>
2021-06-23 13:52:25 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -9,62 +9,78 @@ import time
 
 def timer():
     print("test")
+    #timer will go here once we learn how to use libaries
+    
     
  
 def start():
-            start = input("")
-            if (start == "" or start == " "):
-             timer()
-            else:
-             print("Please press space to start.")
-             start()   
+    start = input("")                   #having no characters as an input ensures that user can just press space.
+    if (start == "" or start == " "):   
+        timer()
+    else:
+        print("Please press space to start.")       #gives a little bit for interactivity to the user, allowing them to choose when to start.
+        start()   
 
 def breakCalc():
     #print(timeTotal)
-    breakAmt = int(timeTotal / 30)
-    longBreakAmt = int(breakAmt / 4)
+    breakAmt = int(timeTotal / 30)              #calculates the amount of total breaks
+    longBreakAmt = int(breakAmt / 4)            #calculates the amount of long breaks
     #print(breakAmt)
-    if (breakAmt >= 4):
+    if (breakAmt >= 4):                         #checks if the user gets a long break or not
         print("You will have", breakAmt, "breaks every twenty-five minutes, where", longBreakAmt, "of them is a 15 minute break, and the rest are five.")
         print("Press enter to start the timer")
-        start() 
+        start()                                #calls the function that starts the timer
     elif (breakAmt < 4):
         print("You will have", breakAmt, "five minute breaks every 25 minutes.")
         print("Press enter to start the timer")
-        start()
+        start()                                #calls the function that starts the timer
 
 def setup():
     print('Hello and welcome to the Study Pomodoro Timer! How long are you studying for?')
-    studyTimeHours = int(input("Hours: ")) 
+    studyTimeHours = int(input("Hours: ")) #asks user for how many hours they want to study for.
     if (studyTimeHours == 1):
-        plural = 'hour'
+        plural = 'hour'                     #checks for proper grammar
     else:
-        plural = 'hours'
+        plural = 'hours'                    #checks for proper grammar
 
-    print(studyTimeHours, plural, 'huh? And how many minutes?')
+    print(studyTimeHours, plural, 'huh? And how many minutes?') #asks user for how many minutes to study
     studyTimeMinutes = int(input('Minutes: '))
     if (studyTimeMinutes == 1):
-     plural2 = 'minute'
+     plural2 = 'minute'                         #checks for proper grammar
     else:
-     plural2 = 'minutes'
+     plural2 = 'minutes'                        #checks for proper grammar
 
-    print(studyTimeMinutes, plural2, "? Alright! And how many seconds?")
+    if (studyTimeMinutes >= 60):                #if the amount of minutes is over 60, it adds 1 hour.
+        hours = int(studyTimeMinutes / 60)
+        remainder = (studyTimeMinutes % 60)
+        studyTimeHours = (studyTimeHours + hours)
+        studyTimeMinutes = 0 + remainder
+
+    print(studyTimeMinutes, plural2, "? Alright! And how many seconds?") #asks for how many seconds to stud
     studyTimeSeconds = int(input("Seconds: "))
     if (studyTimeSeconds == 1):
-        plural3 = "second"
+        plural3 = "second"              #checks for proper grammar
     else:
-        plural3 = "seconds"
-    
-    timeTotal = studyTimeMinutes  + (studyTimeHours * 60)
+        plural3 = "seconds"             #checks for proper grammar
     
     
-    print("Okay! You want to study for ", studyTimeHours, plural, ",", studyTimeMinutes, plural2, ", and", studyTimeSeconds, plural3, "right?")
-    confirmation = input("Yes or No?").lower()
-    if confirmation in ["yes", "y"]: 
-        breakCalc()
+    if (studyTimeSeconds >= 60):            #if the amount of seconds is over 60, it adds one minute
+        minutes = int(studyTimeSeconds / 60)
+        remainder = (studyTimeSeconds % 60)
+        studyTimeMinutes = (studyTimeMinutes + minutes)
+        studyTimeSeconds = 0 + remainder
+    
+    global timeTotal                    #for some reason my code only works if this is called here. Sorry!
+    timeTotal = studyTimeMinutes  + (studyTimeHours * 60)   #Adds the total time studied in minutes
+    
+    
+    print("Okay! You want to study for ", studyTimeHours, plural, ",", studyTimeMinutes, plural2, ", and" studyTimeSeconds, plural3, "right?")
+    confirmation = input("Yes or No? ").lower()#asks for user confirmation, in case they messed up a step. ensures input is not case sensitive
+    if confirmation in ["yes", "y"]:             
+        breakCalc()                            #calls the function which calculates how many breaks user will get
     elif confirmation in ["no", "n"]:
         print("Alright then, let's do this again.")
-        setup()
+        setup()                                #calls back to the start of this function, in case user wants to start over.
  
 setup()
 
